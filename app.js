0876a050c1fd2d6e77e026491cd238747d03de1c
K-ash-ish <kashishsondhiya@gmail.com>
2022-10-12 13:00:06 +0530

    simple login register page

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+const User = new mongoose.model('User', userSchema);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +25,38 @@ app.get('/register', function(req, res){
 app.get('/login', function(req, res){
     res.render('login');
 });
+
+app.post('/register', function(req, res){
+    const user = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    user.save(function(err){
+        if(!err){
+            res.render('secrets');
+        }
+        else{
+            console.log(err);
+        }
+    });
+});
+
+app.post('/login', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username}, function(err, user){
+        if(!err){
+            if(user){
+                if(user.password === password){
+                    res.render('secrets');
+                }
+            }
+        }
+        else{
+            console.log(err);   
+        }
+    })
+})
 
 app.listen(port, function(req, res){
     console.log('Server Running ');
