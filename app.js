9b144db5fb230229cdd3602042cf612c4cbffee6
K-ash-ish <kashishsondhiya@gmail.com>
2022-10-13 11:32:49 +0530

    bcrypt salting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRound = 10;
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -30,28 +31,32 @@ app.get("/login", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-  const user = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-  user.save(function (err) {
-    if (!err) {
-      res.render("secrets");
-    } else {
-      console.log(err);
-    }
+  bcrypt.hash(req.body.password, saltRound, function(err, hash){
+    const user = new User({
+      email: req.body.username,
+      password: hash
+    });
+    user.save(function (err) {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        console.log(err);
+      }
+    });
   });
 });
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
-  User.findOne({ email: username }, function (err, user) {
+  const password = req.body.password;
+  User.findOne({ email: username }, function (err, userPass) {
     if (!err) {
-      if (user) {
-        if (user.password === password) {
-          res.render("secrets");
-        }
+      if (userPass) {
+        bcrypt.compare(password, userPass.password, function(err, result){
+          if(result){
+            res.render("secrets");
+          }
+        })
       }
     } else {
       console.log(err);
